75699919d73f189261946e564501c4d6b98d7161
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-25 16:32:51 +0530

    Add HTML form to accept and display user name via PHP

---
+++ Q2.php
@@ -0,0 +1,16 @@
+2)Create a simple HTML form and accept the user name and display the name through PHP echo statement.
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get"></form>
+    <input type="text" name="User name">
+    <input type="sumbit" name="submit">
+    
+</body>
+</html>
