dead9e674ba6aafca733665819be4aef8cede3a8
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-25 16:20:54 +0530

    Add initial PHP document to display name in various HTML elements

---
+++ Q1.php
@@ -0,0 +1,20 @@
+Display your name Using PHP in a PHP Document in the following places
+
+// 1. In the title tag
+// 2. In the h1 tag
+// 3. In the paragraph tag
+// 4. In the link tag
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DTK TV</title>
+</head>
+<body>
+    <h1>DTK TV</h1>
+    <p>DTK Tv</p>
+    <a href="#">DTK TV</a>
+</body>
+</html>
